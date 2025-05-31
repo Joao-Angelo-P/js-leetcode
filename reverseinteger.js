48123d89c72b144bc0194c10a545b60993e664ea
Joao Angelo <78273535+Joao-Angelo-P@users.noreply.github.com>
2025-05-31 18:21:04 -0400

    Update reverseinteger.js
    second solution

--- reverseinteger.js
+++ reverseinteger.js
@@ -29,3 +29,28 @@ var reverse = function (x) {
     return r;
   }
 };
+
+
+/**
+ * @param {number} x
+ * @return {number}
+ */
+var reverse = function (x) {
+  let n = false;
+
+  if (x < 0) {
+    n = true;
+    x *= -1;
+  }
+
+  x = x.toString();
+  let invertido = x.split("").reverse().join("");
+  let result = n ? "-" + invertido : invertido;
+  result = +result;
+
+  if (result < -1 * 2 ** 31 || result > 2 ** 31 - 1) {
+    return 0;
+  } else {
+    return result;
+  }
+};
